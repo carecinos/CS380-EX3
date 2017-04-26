12215c05e37f463ccb8503e49414b82f6fcf32ef
carecinos <carecinos@cpp.edu>
2017-04-25 18:57:31 -0700

    Initial Commit

---
+++ CS380-EX3/src/Ex3Client.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author cesar
+ *
+ */
+public class Ex3Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
