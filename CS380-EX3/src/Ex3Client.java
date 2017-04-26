368d89de9db5686578edbd1041287a97723e6ad7
carecinos <carecinos@cpp.edu>
2017-04-25 22:51:44 -0700

    Source code commented.

--- CS380-EX3/src/Ex3Client.java
+++ CS380-EX3/src/Ex3Client.java
@@ -8,36 +8,43 @@ import java.net.Socket;
  */
 
 /**
+ * This program creates a Socket connection to codebank.xyz port number
+ * 38103. It then reads a byte of data that represents the number of bytes
+ * that will follow. Once all bytes of data are read, the checksum is calculated
+ * and sent to the server. 
+ * 
  * @author cesar
  *
  */
 public class Ex3Client {
 
 	/**
+	 * This main method saves the bytes of data into an array and
+	 * passes that array into the checkSum method. Then sends the 
+	 * value to the server in two byte sequences.
 	 * @param args
 	 * @throws IOException 
-	 * @throws UnknownHostException 
 	 */
 	public static void main(String[] args) throws  IOException {
 		// TODO Auto-generated method stub
-		try(Socket socket = new Socket("Codebank.xyz", 38103)){
+		try(Socket socket = new Socket("Codebank.xyz", 38103)){	//Create Socket Connection
 			
 			System.out.println("Connected to server.");
 			
 			InputStream is = socket.getInputStream();
 			OutputStream os = socket.getOutputStream();
 			
-			int numOfBytes = is.read();
+			int numOfBytes = is.read();	//First byte is the number of bytes to be received
 			
 			System.out.println("Reading "+ numOfBytes+ " bytes.");
 			byte[] byteArray = new byte[numOfBytes];
 			
-			for(int i = 0; i<byteArray.length; i++){
+			for(int i = 0; i<byteArray.length; i++){	//Save data in an array
 				byteArray[i]= (byte) is.read();
 
 			}
 			
-			System.out.print("Data Received: ");
+			System.out.print("Data Received: "); //Display bytes received 
 			String byteString;
 			for(int i = 0; i<byteArray.length; i++){
 				byteString = "";
@@ -51,22 +58,29 @@ public class Ex3Client {
 			System.out.println();
 			
 			short checksum = checksum(byteArray);//Calculates Checksum
+			//Display checksum
 			System.out.println("Checksum Calculated: 0x" + String.format("%04X", checksum & 0xFFFF));
 			
-			byte[] outArray = new byte[2];
+			byte[] outArray = new byte[2];	//Send checksum in two byte sequences
 			
 			outArray[0] = (byte)((checksum & 0xFF00)>>>8);
 			outArray[1] = (byte)(checksum & 0x00FF);
 			
-			os.write(outArray);
+			os.write(outArray);	//Send checksum
 			
-			if(is.read() == 1)
+			if(is.read() == 1)	//Server Response
 				System.out.println("Response good.");			
 		}
 		System.out.println("Disconnected from Server.");
 
 	}
 	
+	/**
+	 * This method calculates the checksum of the bytes of
+	 * data received from the server.
+	 * @param bytes This is the array of bytes of data
+	 * @return short This is the checksum value.
+	 */
 	public static short checksum(byte[] bytes){
 		long sum = 0;
 		
